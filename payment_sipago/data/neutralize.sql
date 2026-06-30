59df8d95088ec707e520e0576a13429c4e1d6a9e
Ezequiel Ludueña <ezeluduena123@gmail.com>
2026-06-30 12:35:13 -0300

    fix: neutralize.sql debe limpiar todas las credenciales de Sipago
    Solo se neutralizaba sipago_access_token, pero client_id, client_secret
    y sipago_access_token_expiration tambien son credenciales sensibles que
    deben scrubberse al neutralizar la BD para demo/replicacion.

--- payment_sipago/data/neutralize.sql
+++ payment_sipago/data/neutralize.sql
@@ -1,3 +1,6 @@
 -- disable sipago payment provider
 UPDATE payment_provider
-SET sipago_access_token = NULL;
+   SET sipago_client_id = NULL,
+       sipago_client_secret = NULL,
+       sipago_access_token = NULL,
+       sipago_access_token_expiration = NULL;
